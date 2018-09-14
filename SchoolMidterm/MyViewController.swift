5adcddd6994df8beae0bf4f6828c0f0ddec5eb27
Spoke <spoke@Spokede-MacBook-Air.local>
2018-09-14 13:39:07 +0800

    Implement the volume forward backward and play method

--- SchoolMidterm/MyViewController.swift
+++ SchoolMidterm/MyViewController.swift
@@ -17,12 +17,23 @@ class MyViewController: UIViewController {
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var totalTimeLabel: UILabel!
     @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var volumeButton: UIButton!
     
-    var player: AVPlayer!
-    var playerLayer: AVPlayerLayer!
+    
+    var player: AVPlayer?
+    var playerLayer: AVPlayerLayer?
+    var isPlaing = false
+    var isMuted = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer?.frame = videoView.bounds
 
     }
     
@@ -35,24 +46,24 @@ class MyViewController: UIViewController {
         if let searchUrl = searchTxt.text {
             let url = URL(string: "\(searchUrl)")!
             player = AVPlayer(url: url)
-            player.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new, .initial], context: nil)
+            player?.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new, .initial], context: nil)
+            addTimeObserver()
+            playerLayer = AVPlayerLayer(player: player)
+            playerLayer?.videoGravity = .resize
+            videoView.layer.addSublayer(playerLayer!)
         }
-        
     }
     
     func addTimeObserver() {
         let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
         let mainQueue = DispatchQueue.main
-        
-        player.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue) { [weak self] time in
-            
-            guard let currentItem = self?.player.currentItem else { return }
-            
-            self?.timeSlider.minimumValue = 0
+        player?.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue, using: { [weak self] time in
+            guard let currentItem = self?.player?.currentItem else {return}
             self?.timeSlider.maximumValue = Float(currentItem.duration.seconds)
+            self?.timeSlider.minimumValue = 0
             self?.timeSlider.value = Float(currentItem.currentTime().seconds)
             self?.currentTimeLabel.text = self?.getTimeString(time: currentItem.currentTime())
-        }
+        })
     }
     
     func getTimeString(time: CMTime) -> String {
@@ -70,19 +81,71 @@ class MyViewController: UIViewController {
     }
     
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if player != nil {
+            if keyPath == "duration", let duration = player?.currentItem?.duration.seconds, duration > 0.0 {
+                self.totalTimeLabel.text = getTimeString(time: player!.currentItem!.duration)
+            }
+        }
+    }
+    
+    
     @IBAction func backwardsPressed(_ sender: UIButton) {
+        if player != nil {
+            guard let duration = player?.currentItem?.duration else { return }
+            let currentTime = CMTimeGetSeconds(player!.currentTime())
+            var newTime = currentTime - 10.0
+            if newTime < 0 {
+                newTime = 0
+            }
+            let time: CMTime = CMTimeMake(Int64(newTime*1000), 1000)
+            player?.seek(to: time)
+
+        }
     }
     
     
     @IBAction func playPressed(_ sender: UIButton) {
+        
+        if player != nil {
+            if isPlaing == false {
+                player!.play()
+                isPlaing = true
+                playButton.setImage(#imageLiteral(resourceName: "btn_stop"), for: .normal)
+            } else {
+                player!.pause()
+                isPlaing = false
+                playButton.setImage(#imageLiteral(resourceName: "btn_play"), for: .normal)
+            }
+        }
     }
     
 
     @IBAction func forwardPressed(_ sender: UIButton) {
+        if player != nil {
+            guard let duration = player?.currentItem?.duration else { return }
+            let currentTime = CMTimeGetSeconds(player!.currentTime())
+            let newTime = currentTime + 10.0
+            if newTime < (CMTimeGetSeconds(duration) - 10.0) {
+                let time: CMTime = CMTimeMake(Int64(newTime*1000), 1000)
+                player?.seek(to: time)
+            }
+        }
     }
     
     
-    @IBAction func volunePressed(_ sender: UIButton) {
+    @IBAction func volumePressed(_ sender: UIButton) {
+        if player != nil {
+            if isMuted == false {
+                player?.isMuted = true
+                isMuted = true
+                self.volumeButton.setImage(#imageLiteral(resourceName: "btn_volume_off"), for: .normal)
+            } else {
+                player?.isMuted = false
+                isMuted = false
+                self.volumeButton.setImage(#imageLiteral(resourceName: "btn_volume_up"), for: .normal)
+            }
+        }
     }
     
     
